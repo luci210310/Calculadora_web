235cd473bce384e568bcaf00cb8254b9806914b8
Lucimara_Izidoro_Balduino <lucimara.balduino@escola.pr.gov.br>
2025-10-16 08:10:21 -0300

    Add files via upload

--- #include <stdio.c
+++ #include <stdio.c
@@ -1,41 +1,31 @@
-#include <stdio.h>
-#include <time.h> // NecessÃƒÂ¡rio para obter a data/hora atual
+#include <stdio.h> 
+int main() { 
+    float salario1, salario2, salario3;
+    float soma, soma_com_acrescimo; 
+    
+    // Entrada de dados 
+    printf("Digite o primeiro salario: ");
+     scanf("%f", &salario1);
 
-int main() {
-    // VariÃƒÂ¡veis para Nome e Ano de Nascimento
-    char nome[50];
-    int ano_nascimento;
-
-    // --- Obter o Ano atual ---
-    time_t t = time(NULL); // ObtÃ©m o tempo atual
-    // Converte o tempo para a estrutura tm local
-    struct tm *agora = localtime(&t);
-    // Extrai o ano atual (agora->tm_year Ã© anos
-    // desde 1900, entÃ£o somamos 1900)
-    int ano_atual = agora->tm_year + 1900;
-    int idade;
-
-    // --- Captura de Dados ---
-
-    // 1. Solicita e lÃª o nome
-    printf("Digite seu nome:");
-    scanf("%49s", nome);
-
-    // 2. Solicita e lÃª o ano de nascimento
-    printf("Digite o ano do seu nascimento (ex: 1900): ");
-    scanf("%d", &ano_nascimento);
-
-    // --- CÃƒÂ¡lculo da Idade ---
-
-    // A idade ÃƒÂ© calculada subtraindo o ano de nascimento 
-    //do ano atual
-    idade = ano_atual - ano_nascimento;
-
-    //--- Impressão da mensagem ---
-
-    //Imprime a saudação com a idade calculada 
-    // Importante: Este calculo é aproximado. A idade rea 
-    // só é alcançada 
-    // na data de aniversário dentro do ano atual.
-    printf("\nBom dia %s, você tem %d anos.\n", nome, idade)
-}
+      printf("Digite o segundo salario: ");
+       scanf("%f", &salario2);
+       
+        printf("Digite o terceiro salario: ");
+         scanf("%f", &salario3);
+         
+          // Soma inicial 
+          soma = salario1 + salario2 + salario3;
+           // Acrescenta 10% ao segundo e ao terceiro salÃ¡rio
+            salario2 = salario2 + (salario2 * 0.10);
+             salario3 = salario3 + (salario3 * 0.10); 
+             
+             // Soma apÃ³s o acrÃ©scimo 
+             soma_com_acrescimo = salario1 + salario2 + salario3; 
+             
+             // SaÃ­da dos resultados
+              printf("\nSoma inicial dos salarios: R$ %.2f\n", soma);
+               printf("Segundo salario com 10%% de aumento: R$ %.2f\n", salario2);
+    printf("Terceiro salario com 10%% de aumento: R$ %.2f\n", salario3);
+    printf("Soma apos os aumentos: R$ %.2f\n", soma_com_acrescimo);
+     return 0;
+  }
