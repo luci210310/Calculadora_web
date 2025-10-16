9503e5b63f9976b527da7cba2e36a2f47088b4b2
Lucimara_Izidoro_Balduino <lucimara.balduino@escola.pr.gov.br>
2025-10-16 10:57:10 -0300

    Add files via upload

---
+++ #include <stdio.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    // Declaração das variáveis para os salários e a soma 
+    float salario1, salário2, salário3;
+    float somaSalários, novoSalário2;
+
+    // 1. capturar os 3 salarios 
+    printf("Digite o primeiro salario: R$ ");
+    // Usamos %f para ter um número de ponto flutuante (float)
+    If (scanf("%f", &salariol) 1= 1) 
+        printf(*Erro ao ler o primeiro salario. \n*) ;
+        return 1: // Retorna um código de erro
+    }
+
+    printf("Digite o segundo salario: R$ "):
+    1f (scanf ("%f", &salario2) 1= 2)1 {
+    printf("Erro ao ler o segundo salario. \n") ;
+    return 1:|
+    }
+
+printf ("Digite o terceiro salario: R$ *);
+1f (scanf ("f", Gsalario3) 1a 11 (
+printf(*Erro ao ler o terceiro salario.\n*):
+retum 1;
+28
+2303
+// 2. Acrescentar 10% ao segundo salÃ¡rto
+
+// 10% de salario2 Ã© igual a salario2 â€¢ 6:10 // 0 novo salario2 Ã© o salario2 original + 10%
+Enviado do meu iPhone
