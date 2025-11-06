2577c7eef6bc702ec8bd0980c634070e332a54ea
Lucimara_Izidoro_Balduino <lucimara.balduino@escola.pr.gov.br>
2025-11-06 10:55:04 -0300

    Add files via upload

--- # Include <stdio.c
+++ # Include <stdio.c
@@ -1,32 +1,30 @@
 # Include <stdio.h>
 
-// Função pra calcular a média das notas 
-float calcular_media (float nota1, float nota2, float nota3) {
-    return (nota1 + nota2 + nota3) / 3;
+int main() {
+    int opcao, quantidade;
+    float total= 0, parcial;
+
+    printf("===Lanchonte da escola ===\n");
+
+    do {
+        printf("\nCárdapio:\n");
+        printf("1-Cachorro Quente (R$5,00)\n");
+        printf("2-X-Salada  (r$8,00)\n");
+        printf("3-Refrigerante (r$4,00\n");
+        print("4-Suco Natural (r$6,00)\n");
+        printf("0-Finaliar pedido\n");
+
+        printf("\nEscolha o item: ");
+        scanf("%d", &opcao);
+
+        if (opcao ==0) break;
+
+        printf("informe a quantidade: ");
+        scanf("%d", &quantidade);
+        switch(opcao) {
+            case 1:
+            pacial= quantidade * 5.0;
+            printf("Você pediu %d Cachorro(s) Quente: R$ %.2F\n", quantidade, parcial);
+
+        }
 }
-
-//função par determinar se o aluno passou ou não 
-cast char* determinar__aprovação (float media) {
-    if (media > = 6.0) {
-        return "aprovado";
-    } else {
-        return "reprovado";
-    }
-}
-
-Int main () {
-    float nota1, nota2, nota3, media;
-
-    //Entrada e dados
-    printf ("Digite as três notas do aluno");
-    Scanf ("%f%f%f", & nota1, & nota2, & nota3);
-    
-   // cálculo da média 
-   média = calcular_media (nota1, nota2, nota3);
-
-   // Determine e mostrar reultado
-   prinf ("A média das nota é: % 2f\n", media);
-   prinf ("Resultado % s\n", determinar_aprovação(meia));
-
-   return 0;
-    }
